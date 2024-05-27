c565afe8207acfe92caeec707d4aa1350ac25338
André Felipe <140921456+andref03@users.noreply.github.com>
2024-05-27 18:55:35 -0300

    corrigindo bugs e melhorando legibilidade na saída

--- src/main/java/br/ifnmg/edu/poo/passeiocavalosemheuristica/PasseioCavaloSemHeuristica.java
+++ src/main/java/br/ifnmg/edu/poo/passeiocavalosemheuristica/PasseioCavaloSemHeuristica.java
@@ -13,27 +13,27 @@ public class PasseioCavaloSemHeuristica {
             }
         }
     }
-    
+
     private static int realizaPasseio(int linhaAtual, int colunaAtual, int[][] tabuleiro) {
-        
+
         int[] horizontal = {2, 1, -1, -2, -2, -1, 1, 2};
         int[] vertical = {-1, -2, -2, -1, 1, 2, 2, 1};
-        
+
         boolean quadradoValido = true; // booleano p/ impedir laço while infinito
-        
-        int contador = 0;
-        
-        tabuleiro[linhaAtual][colunaAtual] = 0;
-        
+
+        int contador = 1;
+
+        tabuleiro[linhaAtual][colunaAtual] = 1;
+
         while (contador <= 64 && quadradoValido) {
             int proxLinha;
             int proxColuna;
             quadradoValido = false;
-            
+
             for (int moveNumber = 0; moveNumber < 8; moveNumber++) {
                 proxLinha = linhaAtual + vertical[moveNumber];
                 proxColuna = colunaAtual + horizontal[moveNumber];
-                
+
                 if (proxLinha >= 0 && proxLinha < 8 && proxColuna >= 0 && proxColuna < 8 && tabuleiro[proxLinha][proxColuna] == 0) {
                     linhaAtual = proxLinha;
                     colunaAtual = proxColuna;
@@ -44,33 +44,33 @@ public class PasseioCavaloSemHeuristica {
                 }
             }
         }
-        
+
         return contador;
     }
-    
+
     private static void imprimeTabuleiro(int[][] tabuleiro) {
         System.out.println("> Tabuleiro:");
         System.out.println();
         for (int[] linha : tabuleiro) {
             for (int elemento : linha) {
-                System.out.printf("%2d ", elemento);
+                System.out.printf("(%2d) ", elemento);
             }
-             System.out.println();
+            System.out.println();
         }
     }
-    
+
     public static void main(String[] args) {
-        
+
         int[][] tabuleiro = new int[8][8];
 
         inicializaTabuleiro(tabuleiro);
-        
+
         int qtddMovimentos = realizaPasseio(0, 0, tabuleiro); // começa na posição tabuleiro[0][0]
-        
+
         imprimeTabuleiro(tabuleiro);
-        
+
         System.out.println();
-        System.out.println(">> O cavalo realizou " + qtddMovimentos + " movimentos.");
+        System.out.println(">> O cavalo realizou " + (qtddMovimentos - 1) + " movimentos.");
         System.out.println();
         System.out.println("*Obs.: casas NAO visitadas = {0}");
 
